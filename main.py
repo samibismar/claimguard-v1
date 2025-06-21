d6fff736df8be008aad7ea5a312e1a074f9cca49
Sami Bismar <samibismar@icloud.com>
2025-06-21 11:22:36 -0500

    Enable CORS for frontend API access

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import joblib
 import pandas as pd
@@ -20,6 +21,15 @@ class Claim(BaseModel):
 
 # Create API
 app = FastAPI()
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/predict")
 def predict(claim: Claim):
